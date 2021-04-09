393ec19c84531ef66eea7f2ca2e6cb1bcdf47425
artemVash <p.l.a.y@mail.ru>
2021-04-09 17:38:39 +0300

    Логика

--- app/src/main/java/by/vashkevich/calculator/MainActivity.kt
+++ app/src/main/java/by/vashkevich/calculator/MainActivity.kt
@@ -2,6 +2,7 @@ package by.vashkevich.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.*
 import com.google.android.material.textfield.TextInputEditText
 
@@ -14,7 +15,8 @@ class MainActivity : AppCompatActivity() {
         val interestRate = findViewById<TextInputEditText>(R.id.interestRate)     // процент
         val initialCapital = findViewById<TextInputEditText>(R.id.initialCapital) // начальная сумма
         val finalCapital = findViewById<TextInputEditText>(R.id.finalCapital)     // ожидаеммая сумма
-        var sumMonth: EditText             // пополнение в месяц
+        var sumMonth = findViewById<TextInputEditText>(R.id.sumMonth)             // пополнение в месяц
+
 
         //Поля RadioButton
         val radioButtonWeek = findViewById<RadioButton>(R.id.radioButtonWeek)
@@ -23,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         // Поле CheckBox
         val replenishmentCheckBox = findViewById<CheckBox>(R.id.replenishmentCheckBox)
 
+
         //Поле вывода
         var textCalculation = findViewById<TextView>(R.id.textCalculation)
 
@@ -30,44 +33,59 @@ class MainActivity : AppCompatActivity() {
         val buttonCalculation = findViewById<Button>(R.id.buttonCalculation)
 
 
+        replenishmentCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked){
+                sumMonth.isEnabled = true
+            }else{
+                sumMonth.isEnabled = false
+                sumMonth.text?.clear()
+            }
+        }
+
 
         buttonCalculation.setOnClickListener {
-
-            val numInterestRate = interestRate.text.toString().toDouble()
-            var numInitialCapital = initialCapital.text.toString().toDouble()
-            var numFinalCapital = finalCapital.text.toString().toDouble()
 
             var daysCounter = 0
             var weeks = 0.0
 
+            if (initialCapital.text.isNullOrEmpty() ||
+                    interestRate.text.isNullOrEmpty() ||
+                    finalCapital.text.isNullOrEmpty()) {
+                Toast.makeText(this, "Заполните все поля", Toast.LENGTH_LONG).show()
+            }else{
 
+                if(radioButtonMonth.isChecked || radioButtonWeek.isChecked) {
 
-            if (numInitialCapital != null && numInterestRate != null && numFinalCapital != null) {
-                while (numInitialCapital < numFinalCapital!!) {
+                    val numInterestRate = interestRate.text.toString().toDouble()
+                    var numInitialCapital = initialCapital.text.toString().toDouble()
+                    val numFinalCapital = finalCapital.text.toString().toDouble()
 
-                    daysCounter++
+                    while (numInitialCapital < numFinalCapital) {
 
-                    if (daysCounter % 30 == 0 && radioButtonMonth.isChecked) {
-                        numInitialCapital += numInitialCapital / 100 * numInterestRate!! // ежемесечная капитализация
-                        weeks += 4.3; // 4.3 количество недель в месяце
-                    } else if (daysCounter % 7 == 0 && radioButtonWeek.isChecked) {
-                        numInitialCapital += numInitialCapital / 100 * numInterestRate!! // еженедельная капитализация
-                        weeks++;
+                        daysCounter++
+
+                        if (daysCounter % 30 == 0 && radioButtonMonth.isChecked) {
+                            numInitialCapital += numInitialCapital / 100 * numInterestRate // ежемесечная капитализация
+                            weeks += 4.3; // 4.3 количество недель в месяце
+                        } else if (daysCounter % 7 == 0 && radioButtonWeek.isChecked) {
+                            numInitialCapital += numInitialCapital / 100 * numInterestRate // еженедельная капитализация
+                            weeks++;
+                        }
+
+                        if (replenishmentCheckBox.isChecked && daysCounter % 30 == 0) {
+                            val numSumMonth = (sumMonth.text.toString()).toDouble()
+                            numInitialCapital += numSumMonth;
+                        }
+
                     }
-
-                    if (replenishmentCheckBox.isChecked && daysCounter % 30 == 0) {
-                        sumMonth = findViewById<TextInputEditText>(R.id.sumMonth)
-                        val numSumMonth = (sumMonth.text.toString()).toDouble()
-                        numInitialCapital += numSumMonth;
-                    }
-
+                }else{
+                    Toast.makeText(this, "Выберите тип капитализации", Toast.LENGTH_LONG).show()
                 }
             }
             val month = weeks / 4.3
 
-
-
-            textCalculation.text = "$month Месяцев "
+            val monthEnd: String = String.format("%.2f", month)
+            textCalculation.text = "$monthEnd Месяцев "
 
         }
 
