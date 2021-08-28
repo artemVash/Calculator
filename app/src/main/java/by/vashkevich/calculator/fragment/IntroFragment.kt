fd1a9900edf66f1d890478517dfc0b6bf3a60d2e
artemVash <p.l.a.y@mail.ru>
2021-08-28 12:02:35 +0300

    layout all screens

--- app/src/main/java/by/vashkevich/calculator/fragment/IntroFragment.kt
+++ app/src/main/java/by/vashkevich/calculator/fragment/IntroFragment.kt
@@ -1,13 +1,11 @@
 package by.vashkevich.calculator.fragment
 
 import android.os.Bundle
-import android.transition.TransitionInflater
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
 import by.vashkevich.calculator.R
